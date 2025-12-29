f68cd8058bdcbc0040b2a0394d188769767da203
Vlad Sharapa <sharapa.primary@gmail.com>
2025-12-29 21:35:11 +0300

    refactor: change custom user exceptions

--- app/src/users/exceptions.py
+++ app/src/users/exceptions.py
@@ -1,16 +1,26 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, status
+
+from app.exceptions import BaseHttpApplicationException
 
 
-class UserAlreadyExistsException(HTTPException): ...
+class UserAlreadyExistsException(BaseHttpApplicationException):
+    status_code = status.HTTP_409_CONFLICT
+    detail = "User with this email already exists"
 
 
-class UserNotExistsException(HTTPException): ...
+class UserNotExistsException(BaseHttpApplicationException):
+    status_code = status.HTTP_404_NOT_FOUND 
+    detail = "User does not exist"
 
 
-class UserAlreadyBlockedException(HTTPException): ...
+class UserAlreadyBlockedException(BaseHttpApplicationException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    detail = "User is already blocked"
 
 
-class UserAlreadyActiveException(HTTPException): ...
+class UserAlreadyActiveException(BaseHttpApplicationException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    detail="User is already active"
 
 
 class NegativeBalanceException(HTTPException): ...
