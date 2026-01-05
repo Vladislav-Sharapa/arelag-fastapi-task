f89474b8f522538d301002b75d080738d3283e9e
Vlad Sharapa <sharapa.primary@gmail.com>
2026-01-05 22:26:50 +0300

    style: apply formatters

--- app/src/users/exceptions.py
+++ app/src/users/exceptions.py
@@ -9,7 +9,7 @@ class UserAlreadyExistsException(BaseHttpApplicationException):
 
 
 class UserNotExistsException(BaseHttpApplicationException):
-    status_code = status.HTTP_404_NOT_FOUND 
+    status_code = status.HTTP_404_NOT_FOUND
     detail = "User does not exist"
 
 
@@ -20,7 +20,7 @@ class UserAlreadyBlockedException(BaseHttpApplicationException):
 
 class UserAlreadyActiveException(BaseHttpApplicationException):
     status_code = status.HTTP_400_BAD_REQUEST
-    detail="User is already active"
+    detail = "User is already active"
 
 
 class NegativeBalanceException(HTTPException): ...
--- app/src/users/router.py
+++ app/src/users/router.py
@@ -3,8 +3,7 @@ import typing
 from fastapi import APIRouter, Depends, status
 
 from app.src.users.dependencies import get_user_service
-from app.src.users.schemas import (RequestUserModel, RequestUserUpdateModel,
-                                   ResponseUserModel, UserFilter, UserModel)
+from app.src.users.schemas import RequestUserModel, RequestUserUpdateModel, ResponseUserModel, UserFilter, UserModel
 from app.src.users.service import UserService
 
 router = APIRouter()
@@ -26,31 +25,3 @@ async def post_user(user: RequestUserModel, service: UserService = Depends(get_u
 @router.patch("/users/{user_id}", response_model=typing.Optional[UserModel] | None)
 async def patch_user(user_id: int, user: RequestUserUpdateModel, service: UserService = Depends(get_user_service)):
     return await service.patch_user(id=user_id, user=user)
-
-
-# @router.patch("/users/{user_id}", response_model=typing.Optional[UserModel] | None)
-# async def patch_user(user_id: int, user: RequestUserUpdateModel, session: AsyncSession = Depends(get_async_session)):
-#     if user_id < 0:
-#         raise BadRequestDataException(
-#             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Unprocessable data in request"
-#         )
-#     db_user = await session.execute(select(User).where(User.id == user_id))
-#     db_user = db_user.scalar()
-#     if not db_user:
-#         raise UserNotExistsException(
-#             status_code=status.HTTP_404_NOT_FOUND, detail="User with id=`{0}` does not exist".format(user_id)
-#         )
-#     if db_user.status == "BLOCKED" and user.status == "BLOCKED":
-#         raise UserAlreadyBlockedException(
-#             status_code=status.HTTP_400_BAD_REQUEST, detail="User with id=`{0}` is already blocked".format(user_id)
-#         )
-#     if db_user.status == "ACTIVE" and user.status == "ACTIVE":
-#         raise UserAlreadyActiveException(
-#             status_code=status.HTTP_400_BAD_REQUEST, detail="User with id=`{0}` is already active".format(user_id)
-#         )
-#     await session.execute(update(User).values(**{"status": user.status}).where(User.id == user_id))
-#     await session.commit()
-#     user = await session.execute(select(User).where(User.id == user_id))
-#     user = user.scalar()
-#     result = UserModel(id=user.id, email=user.email, status=UserStatusEnum(user.status), created=user.created)
-#     return result
