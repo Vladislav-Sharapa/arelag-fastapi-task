57f9a75f892f4c9c7e6de34c73adf6ec210d51a4
Vlad Sharapa <sharapa.primary@gmail.com>
2026-01-09 20:28:46 +0300

    refactor: refactoring transaction model

--- app/model.py
+++ app/model.py
@@ -6,4 +6,5 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
     id: Mapped[int] = mapped_column(primary_key=True)
-    created: Mapped[datetime] = mapped_column(server_default=func.now())
+    created: Mapped[datetime] = mapped_column(default=datetime.utcnow()) #TODO Change later to server_default=func.now()
+
--- app/src/transactions/models.py
+++ app/src/transactions/models.py
@@ -1,13 +1,15 @@
-from sqlalchemy import Column, DateTime, Integer, Numeric, String
+from decimal import Decimal
+
+from sqlalchemy import Column, Integer, Numeric, String
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app.model import Base
 
 
 class Transaction(Base):
     __tablename__ = "transaction"
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, nullable=False)
-    currency = Column(String, nullable=True)
-    amount = Column(Numeric, nullable=True)
-    status = Column(String, nullable=True)
-    created = Column(DateTime, nullable=True)
+
+    user_id: Mapped[int] = mapped_column(nullable=False)
+    currency: Mapped[str] = mapped_column(nullable=True)
+    amount: Mapped[Decimal] = mapped_column(nullable=True)
+    status: Mapped[str] = mapped_column(nullable=True)
