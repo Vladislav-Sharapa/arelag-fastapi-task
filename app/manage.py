9b1721d72726ad272c10e5f8a4232f05092a86b8
Vlad Sharapa <sharapa.primary@gmail.com>
2026-01-11 15:48:55 +0300

    fix: correct uvicorn  run target

--- app/manage.py
+++ app/manage.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 
+from app.src.transactions.exceptions import register_transaction_error_hadlers
 from app.src.transactions.router import router as transaction_router
 from app.src.users.router import router as user_router
 
@@ -9,5 +10,7 @@ app = FastAPI()
 app.include_router(transaction_router)
 app.include_router(user_router)
 
+register_transaction_error_hadlers(app)
+
 if __name__ == "__main__":
-    uvicorn.run("manage:app", host="0.0.0.0", port=7999, reload=True)
+    uvicorn.run("app.manage:app", host="0.0.0.0", port=7999, reload=True)
