97c0434cc175714dcafa482f196321f0b212c7c9
Vlad Sharapa <sharapa.primary@gmail.com>
2026-01-11 15:53:58 +0300

    feat: restructure transaction module

---
+++ app/src/transactions/repository.py
@@ -0,0 +1,17 @@
+from typing import List
+
+from sqlalchemy import select
+
+from app.src.transactions.models import Transaction
+from app.utils.repository import SQLAlchemyRepository
+
+
+class TransactionRepository(SQLAlchemyRepository):
+    model = Transaction
+
+    async def get_all_by_user_id(self, user_id: int) -> List[Transaction] | None:
+        query = select(Transaction).filter(Transaction.user_id == user_id)
+        query_result = await self.session.execute(query)
+
+        return query_result.scalars()
+    
