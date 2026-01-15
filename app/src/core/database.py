c7f1e60a2c354b35763af46a4f3a9db33ba81a7d
Vlad Sharapa <sharapa.primary@gmail.com>
2026-01-15 15:57:54 +0300

    feat: use settings to connect to the database and run the application

--- app/src/core/database.py
+++ app/src/core/database.py
@@ -1,9 +1,9 @@
 from typing import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
-                                    create_async_engine)
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from app.src.core.config import config
 
-engine = create_async_engine(url="sqlite+aiosqlite:///db.sqlite3", echo=True)
+engine = create_async_engine(url=config.database.url, echo=True)
 
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
