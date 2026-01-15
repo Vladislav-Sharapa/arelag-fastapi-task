812e7666fb0f153c6c5ff7679f46e1d8c6623dec
Vlad Sharapa <sharapa.primary@gmail.com>
2026-01-15 15:54:19 +0300

    feat: add settings for app and database

---
+++ app/src/core/config.py
@@ -0,0 +1,10 @@
+from app.src.settings.application import ApplicationSetting
+from app.src.settings.database import PostgreSQLSetting
+
+
+class Config:
+    database: PostgreSQLSetting = PostgreSQLSetting()
+    application: ApplicationSetting = ApplicationSetting()
+
+
+config = Config()
