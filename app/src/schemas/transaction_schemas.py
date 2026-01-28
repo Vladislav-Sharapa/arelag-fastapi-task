f578a3c2c738c485e8412bf7875866630b1dcf33
Vlad Sharapa <sharapa.primary@gmail.com>
2026-01-28 23:37:14 +0300

    refactor: [TASK-0011] move validation from endpoint to serializer

--- app/src/schemas/transaction_schemas.py
+++ app/src/schemas/transaction_schemas.py
@@ -1,9 +1,9 @@
-import typing
+from typing import Optional
 from datetime import datetime
 from decimal import Decimal
 from enum import StrEnum
 
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, field_validator
 
 from app.src.core.enums import CurrencyEnum
 
@@ -17,17 +17,23 @@ class RequestTransactionModel(BaseModel):
     currency: CurrencyEnum
     amount: Decimal
 
+    @field_validator("amount")
+    def validate_amount(cls, v):
+        if v == 0:
+            raise ValueError("Transaction can not have zero amount")
+        return v
+
 
 class ResponseTransactionModel(BaseModel):
     pass
 
 
 class TransactionModel(BaseModel):
-    id: typing.Optional[int]
-    user_id: typing.Optional[int] = None
-    currency: typing.Optional[CurrencyEnum] = None
-    amount: typing.Optional[float] = None
-    status: typing.Optional[TransactionStatusEnum] = None
-    created: typing.Optional[datetime] = None
+    id: Optional[int]
+    user_id: Optional[int] = None
+    currency: Optional[CurrencyEnum] = None
+    amount: Optional[float] = None
+    status: Optional[TransactionStatusEnum] = None
+    created: Optional[datetime] = None
 
     model_config = ConfigDict(from_attributes=True)
